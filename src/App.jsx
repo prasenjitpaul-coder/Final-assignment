ba4503ca1df7c7b78891c0724d95d36f8622a0ff
prasenjitpaul-coder <prasenjitpaul.coder@gmail.com>
2026-06-06 13:16:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Navbar from "./component/Navbar.jsx";
+function App(){
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+}
+export default App;
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='navmain'>
+            <a>LOGO</a>
+            <div className='nav2nddiv'>
+            <p>Home</p>
+            <p>How it works</p>
+            <p>Featires</p>
+            <p>Pricing</p>
+            <button>Create Account</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
